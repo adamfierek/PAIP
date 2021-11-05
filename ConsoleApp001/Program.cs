dab566a7676d0b34987ca56457ba53c447a278e6
Adam Fierek <31532851+kolorowezworki@users.noreply.github.com>
2021-11-05 12:31:30 +0100

    Add project files.

---
+++ ConsoleApp001/Program.cs
@@ -0,0 +1,22 @@
+﻿using PAIP2;
+using System;
+
+namespace ConsoleApp001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var paip = new PAIP();
+
+            int.TryParse(Console.ReadLine(), out int i);
+
+            //var result = paip.Factorial(i);
+
+            var result = paip.IsPrime(i);
+
+            Console.WriteLine(result);
+            
+        }
+    }
+}
---
+++ PAIP2/PAIP2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PAIP2
+{
+    public class PAIP
+    {
+        public int Factorial(int a)
+        {
+            var result = 1;
+
+            for (var i = 1; i <= a; i++)
+            {
+                result *= i; // to samo, co result = result * i
+            }
+
+            return result;
+        }
+        public int Factorial2(int a)
+        {
+            var result = 1;
+            var i = 1;
+            while (i <= a)
+            {
+                result *= i;
+                i++;
+            }
+
+            return result;
+        }
+
+        public bool IsPrime(int a) //Ctrl+K, D <- Formatowanie kodu
+        {
+            for (var i = 2; i < a; i++)
+            {
+                if (a % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
