766778a32e88403ba6f429c4771ad851fe22bfa3
Adam Fierek <31532851+kolorowezworki@users.noreply.github.com>
2022-02-14 16:35:19 +0100

    Working WebAPI

---
+++ WebAPI/Controllers/PaipController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PaipController : ControllerBase
+    {
+        [HttpGet] //HttpPost, HttpPut, HttpDelete
+        public IActionResult Test()
+        {
+            return Ok("Witaj API!");
+        }
+    }
+}
